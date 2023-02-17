18a6f889fd877fa9905890d4973b1eed78d8eb45
rizal <rizal.firdaus117@gmail.com>
2023-02-17 13:43:50 +0700

    register provider

--- app/Providers/TodolistServiceProvider.php
+++ app/Providers/TodolistServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace App\Providers;
 
+use App\Services\Imp\TodolistServiceImp;
+use App\Services\TodolistService;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class TodolistServiceProvider extends ServiceProvider
+class TodolistServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+    public array $singletons = [
+        TodolistService::class => TodolistServiceImp::class
+    ];
+
+    public function provides(): array
+    {
+        return [TodolistService::class];
+    }
     /**
      * Register services.
      */
