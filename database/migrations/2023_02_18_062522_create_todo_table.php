1e687bdedf9f63675390939a907d4fd6c1073465
rizal <rizal.firdaus117@gmail.com>
2023-02-18 13:26:21 +0700

    migration db

--- database/migrations/2023_02_18_062522_create_todo_table.php
+++ database/migrations/2023_02_18_062522_create_todo_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("todo");
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todo');
     }
 };
